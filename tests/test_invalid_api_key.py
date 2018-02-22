b0009cf249a1f200f352da6c7dd3151f64fbdff1
augustak <august@lucidtech.io>
2018-02-22 20:02:14 +0100

    test invalid api key

---
+++ tests/test_invalid_api_key.py
@@ -0,0 +1,22 @@
+import pytest
+
+from las import Receipt, Client
+from las.client import InvalidAPIKeyException
+
+
+@pytest.fixture(scope='module')
+def url(params):
+    return params('receipt_url')
+
+
+def test_invalid_api_key(url, client):
+    receipt = Receipt(url=url)
+    client.api_key = 'foo'
+
+    try:
+        client.scan_receipt(receipt)
+        assert False
+    except InvalidAPIKeyException:
+        assert True
+    except Exception:
+        assert False
