f559c5d8ffe9558e22ddb0ec45ec7943484be6ad
augustak <august@lucidtech.io>
2018-03-05 13:05:18 +0100

    updated docs and docstrings

--- las/client.py
+++ las/client.py
@@ -5,7 +5,7 @@ import logging
 from backoff import on_exception, expo
 from urllib.parse import urlencode, quote_plus
 from io import BytesIO
-from .extrahdr import what
+from ._extrahdr import what
 from requests.exceptions import RequestException
 from json.decoder import JSONDecodeError
 
@@ -15,11 +15,11 @@ logger.addHandler(logging.NullHandler())
 logger.setLevel(logging.INFO)
 
 
-def fatal_code(e):
+def _fatal_code(e):
     return 400 <= e.response.status_code < 500
 
 
-def json_decode(response):
+def _json_decode(response):
     try:
         response.raise_for_status()
         return response.json()
@@ -36,44 +36,92 @@ def json_decode(response):
 
 
 class LimitExceededException(Exception):
+    """a LimitExceededException is raised if the number of
+    transactions or receipts exceeds the limit"""
     pass
 
 
 class FileFormatException(Exception):
+    """a FileFormatException is raised if the file format
+    is not supported by the api."""
     pass
 
 
 class InvalidAPIKeyException(Exception):
+    """an InvalidAPIKeyException is raised if api key is invalid."""
     pass
 
 
-class Response:
-    def __init__(self, status_code):
-        self.status_code = status_code
+class _Response:
+    def __init__(self, requests_response):
+        self.requests_response = requests_response
+        self.status_code = requests_response.status_code
 
 
-class ScanResponse(Response):
-    def __init__(self, response):
-        super().__init__(response.status_code)
-        self.detections = json_decode(response)
+class ScanResponse(_Response):
+    """a ScanResponse object contains the result of a successful call
+    to the scan api. Do not create this object on your own, the
+    :py:class:`~las.client.Client` object will do that for you.
+
+    :param requests.Response requests_response: A requests response object.
+    :param detections: A list of detections done on the document.
+    :type detections: list(dict)
+
+    """
+    def __init__(self, requests_response):
+        super().__init__(requests_response)
+        self.detections = _json_decode(requests_response)
 
 
-class MatchResponse(Response):
-    def __init__(self, response):
-        super().__init__(response.status_code)
-        decoded = json_decode(response)
+class MatchResponse(_Response):
+    """a MatchResponse object contains the result of a successful call
+    to the matching api. Do not create this object on your own, the
+    :py:class:`~las.client.Client` object will do that for you.
+
+    :param requests.Response requests_response: A requests response object.
+    :param matched_transactions: A dictionary of matched transaction ids to receipt ids.
+    :type matched_transactions: dict(str, str)
+    :param unmatched_transactions: A list of unmatched transaction ids.
+    :type unmatched_transactions: list(str)
+
+    """
+    def __init__(self, requests_response):
+        super().__init__(requests_response)
+        decoded = _json_decode(requests_response)
         self.matched_transactions = decoded['matchedTransactions']
         self.unmatched_transactions = decoded['unmatchedTransactions']
 
 
 class Client:
+    """a Client object stores configuration state and allows you to invoke
+    api methods from Lucidtech AI Services.
+
+    :param str api_key: Your api key
+    :param str base_endpoint: Domain endpoint of the api
+    :param str stage: Version of the api
+
+    """
     def __init__(self, api_key, base_endpoint='https://api.lucidtech.ai', stage='v0'):
         self.api_key = api_key
         self.base_endpoint = base_endpoint
         self.stage = stage
 
-    @on_exception(expo, RequestException, max_tries=3, giveup=fatal_code)
+    @on_exception(expo, RequestException, max_tries=3, giveup=_fatal_code)
     def scan_receipt(self, receipt):
+        """Scan receipt.
+
+        >>> from las import Client, Receipt
+        >>> client = Client(api_key='<api key>')
+        >>> receipt = Receipt(url='https://example.com/receipt.jpeg')
+        >>> response = client.scan_receipt(receipt)
+
+        :param Receipt receipt: The receipt to scan
+        :return: The results of the scan
+        :rtype: ScanResponse
+        :raises FileFormatException: If the receipt file format is not supported by the api
+        :raises InvalidAPIKeyException: If the api key is invalid
+
+        """
         receipt_id = self._upload_receipt(receipt)
 
         headers = {
@@ -87,8 +135,22 @@ class Client:
         response = requests.post(endpoint, headers=headers)
         return ScanResponse(response)
 
-    @on_exception(expo, RequestException, max_tries=3, giveup=fatal_code)
+    @on_exception(expo, RequestException, max_tries=3, giveup=_fatal_code)
     def scan_invoice(self, invoice):
+        """Scan invoice.
+
+        >>> from las import Client, Invoice
+        >>> client = Client(api_key='<api key>')
+        >>> invoice = Invoice(url='https://example.com/invoice.jpeg')
+        >>> response = client.scan_invoice(invoice)
+
+        :param Invoice invoice: The invoice to scan
+        :return: The results of the scan
+        :rtype: ScanResponse
+        :raises FileFormatException: If the receipt file format is not supported by the api
+        :raises InvalidAPIKeyException: If the api key is invalid
+
+        """
         receipt_id = self._upload_receipt(invoice)
 
         headers = {
@@ -102,8 +164,32 @@ class Client:
         response = requests.post(endpoint, headers=headers)
         return ScanResponse(response)
 
-    @on_exception(expo, RequestException, max_tries=3, giveup=fatal_code)
+    @on_exception(expo, RequestException, max_tries=3, giveup=_fatal_code)
     def match_receipts(self, transactions, receipts, matching_fields, matching_strategy=None):
+        """Matches transactions and receipts
+
+        >>> from las import Client, Receipt
+        >>> client = Client(api_key='<api key>')
+        >>> transactions = {'t1': {'total': '100.34', 'date': '2018-03-04'}}
+        >>> receipts = {'r1': Receipt(url='https://example.com/receipt.jpeg')}
+        >>> matching_fields = ['total', 'date']
+        >>> matching_strategy = {'total': 'maximumDeviation': '2.00'}
+        >>> response = client.match_receipts(transactions, receipts, matching_fields, matching_strategy)
+
+        :param transactions: A dictionary of transaction ids to transaction data
+        :type transactions: dict(str, dict)
+        :param receipts: A dictionary of receipt ids to :py:class:`~las.receipt.Receipt` objects
+        :type receipts: dict(str, Receipt)
+        :param matching_fields: A list of all the fields to check for matching
+        :type matching_fields: list(str)
+        :param matching_strategy: A dictionary of fields to matching strategies for that field
+        :type matching_strategy: dict(str, dict) or None
+        :return: The results of the matching
+        :rtype: MatchResponse
+        :raises FileFormatException: If the receipt file format is not supported by the api
+        :raises InvalidAPIKeyException: If the api key is invalid
+        :raises LimitExceededException: If number of transactions exceeds 100 or number of receipts exceeds 15
+        """
         if len(transactions) > 100:
             raise LimitExceededException('Exceeded maximum of 100 transactions per request')
 
@@ -128,7 +214,7 @@ class Client:
         response = requests.post(endpoint, json=body, headers=headers)
         return MatchResponse(response)
 
-    @on_exception(expo, RequestException, max_tries=3, giveup=fatal_code)
+    @on_exception(expo, RequestException, max_tries=3, giveup=_fatal_code)
     def _upload_receipt(self, receipt):
         supported_formats = {'jpeg', 'png', 'bmp', 'gif', 'pdf'}
         fp = BytesIO(receipt.content)
@@ -142,7 +228,7 @@ class Client:
             endpoint = '/'.join([self.base_endpoint, self.stage, 'receipts/upload'])
 
             response = requests.get(endpoint, headers=headers)
-            decoded = json_decode(response)
+            decoded = _json_decode(response)
 
             upload_url = decoded['uploadUrl']
             receipt_id = decoded['receiptId']
--- las/receipt.py
+++ las/receipt.py
@@ -2,6 +2,16 @@ import requests
 
 
 class Receipt:
+    """a Receipt object contains image or pdf data of a receipt and is
+    typically passed to :py:class:`~las.client.Client` for scanning.
+    Supported file formats are jpeg, png, gif, bmp and pdf.
+
+    :param str url: An http or https url pointing to an receipt image or pdf.
+    :param str filename: A path to a local receipt image or pdf file.
+    :param typing.BinaryIO fp: A file-like python object
+    :param bytes content: Binary receipt image or pdf data.
+
+    """
     def __init__(self, url=None, filename=None, fp=None, content=None):
         if not fp and not url and not filename and not content:
             raise Exception('fp, url, filename or content must be provided')
