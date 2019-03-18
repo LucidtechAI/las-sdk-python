95f6c96041bf3c906594632bcd2870b61cee5467
augustak <august@lucidtech.ai>
2019-03-18 11:55:23 +0100

    Update renamed private methods in Authorization to leading underscore

--- las/authorization.py
+++ las/authorization.py
@@ -20,22 +20,22 @@ class Authorization:
         if not body:
             body = ''.encode('utf-8')
 
-        amz_date, datestamp = self.timestamp()
+        amz_date, datestamp = self._timestamp()
 
-        canonical_request, headers = self.get_canonical_request(
+        canonical_request, headers = self._get_canonical_request(
             uri=uri,
             method=method,
             body=body,
             amz_date=amz_date)
 
         req_digest = hashlib.sha256(canonical_request).hexdigest()
-        cred_scope = self.get_credential_scope(datestamp)
-        string_to_sign = self.get_sign_string(amz_date, cred_scope, req_digest)
-        signing_key = self.get_signature_key(datestamp)
+        cred_scope = self._get_credential_scope(datestamp)
+        string_to_sign = self._get_sign_string(amz_date, cred_scope, req_digest)
+        signing_key = self._get_signature_key(datestamp)
 
         signature = hmac.new(signing_key, string_to_sign, hashlib.sha256).hexdigest()
 
-        auth_header = self.build_auth_header(
+        auth_header = self._build_auth_header(
             amz_date=amz_date,
             access_key=self.credentials.access_key_id,
             api_key=self.credentials.api_key,
@@ -46,17 +46,17 @@ class Authorization:
 
         return auth_header
 
-    def get_sign_string(self, amz_date: str, cred_scope: str, req_digest: str) -> bytes:
+    def _get_sign_string(self, amz_date: str, cred_scope: str, req_digest: str) -> bytes:
         sign_parts = [self.ALGORITHM, amz_date, cred_scope, req_digest]
         return '\n'.join(sign_parts).encode('utf-8')
 
-    def get_credential_scope(self, datestamp: str) -> str:
+    def _get_credential_scope(self, datestamp: str) -> str:
         scope_parts = [datestamp, self.region, self.service, 'aws4_request']
         return '/'.join(scope_parts)
 
-    def get_canonical_request(self, uri: ParseResult, method: str, body: bytes, amz_date: str) \
+    def _get_canonical_request(self, uri: ParseResult, method: str, body: bytes, amz_date: str) \
             -> Tuple[bytes, Iterable[str]]:
-        canonical_querystring = self.get_canonical_querystring(uri.query)
+        canonical_querystring = self._get_canonical_querystring(uri.query)
 
         header_keys = ('host', 'x-amz-date', 'x-api-key')
         header_values = (uri.netloc, amz_date, self.credentials.api_key)
@@ -72,11 +72,11 @@ class Authorization:
 
         return '\n'.join(request_components).encode('utf-8'), headers.keys()
 
-    def get_canonical_querystring(self, querystring: str) -> str:
+    def _get_canonical_querystring(self, querystring: str) -> str:
         # TODO: Fixme.
         return querystring
 
-    def get_signature_key(self, datestamp: str) -> str:
+    def _get_signature_key(self, datestamp: str) -> str:
         def sign(key, msg):
             return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
 
@@ -87,8 +87,8 @@ class Authorization:
         return signature
 
     @classmethod
-    def build_auth_header(cls, amz_date: str, access_key: str, api_key: str, signature: str,
-                          credential_scope: str, signed_headers: Iterable[str]) -> dict:
+    def _build_auth_header(cls, amz_date: str, access_key: str, api_key: str, signature: str,
+                           credential_scope: str, signed_headers: Iterable[str]) -> dict:
         auth = {
             'Credential': f'{access_key}/{credential_scope}',
             'SignedHeaders': ';'.join(signed_headers),
@@ -108,7 +108,7 @@ class Authorization:
         return headers
 
     @staticmethod
-    def timestamp() -> Tuple[str, str]:
+    def _timestamp() -> Tuple[str, str]:
         now = datetime.datetime.utcnow()
         amz_date = now.strftime('%Y%m%dT%H%M%SZ')
         datestamp = now.strftime('%Y%m%d')
