6cee2c398c8172e6010b6900de6c600094970be1
Magnus Rud <mag.aars@gmail.com>
2020-12-07 16:07:55 +0100

    use requests mock

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,35 +1,10 @@
-import requests
+import json
 
+import requests_mock
 import pytest
-
-from unittest.mock import Mock, patch
 
 from las.client import InvalidCredentialsException, LimitExceededException, TooManyRequestsException
 from . import util
-
-
-def create_invalid_credentials_mock():
-    return Mock(
-        json=Mock(return_value={'message': 'Forbidden'}),
-        raise_for_status=Mock(side_effect=requests.HTTPError()),
-        status_code=403,
-    )
-
-
-def create_too_many_requests_mock():
-    return Mock(
-        json=Mock(return_value={'message': 'Too Many Requests'}),
-        raise_for_status=Mock(side_effect=requests.HTTPError()),
-        status_code=429,
-    )
-
-
-def create_limit_exceeded_mock():
-    return Mock(
-        json=Mock(return_value={'message': 'Limit Exceeded'}),
-        raise_for_status=Mock(side_effect=requests.HTTPError()),
-        status_code=429,
-    )
 
 
 @pytest.fixture(scope='module')
@@ -38,19 +13,14 @@ def client_with_access_token(client):
     return client
 
 
-@patch('requests.post')
-@patch('requests.patch')
-@patch('requests.delete')
-@pytest.mark.parametrize('error_mock,error_name', [
-    (create_invalid_credentials_mock(), InvalidCredentialsException),
-    (create_too_many_requests_mock(), TooManyRequestsException),
-    (create_limit_exceeded_mock(), LimitExceededException),
+@pytest.mark.parametrize('error_code,error_content,error_name', [
+    (429, json.dumps({'message': 'Limit Exceeded'}).encode(), LimitExceededException),
+    (429, json.dumps({'message': 'Too Many Requests'}).encode(), TooManyRequestsException),
+    (403, json.dumps({'message': 'Forbidden'}).encode(), InvalidCredentialsException),
 ])
 def test_invalid_credentials(
-    post_mock,
-    patch_mock,
-    delete_mock,
-    error_mock,
+    error_code,
+    error_content,
     error_name,
     content,
     mime_type,
@@ -58,22 +28,24 @@ def test_invalid_credentials(
 ):
 
     client = client_with_access_token
-    delete_mock.return_value = error_mock
-    post_mock.return_value = error_mock
-    patch_mock.return_value = error_mock
-
     consent_id = util.consent_id()
     document_id = util.document_id()
     model_id = util.model_id()
 
-    with pytest.raises(error_name):
-        client.create_document(content, mime_type, consent_id=consent_id)
+    with requests_mock.Mocker() as m:
+        m.post('/'.join([client.endpoint, 'documents']), status_code=error_code, content=error_content)
+        m.post('/'.join([client.endpoint, 'predictions']), status_code=error_code, content=error_content)
+        m.patch('/'.join([client.endpoint, 'documents', document_id]), status_code=error_code, content=error_content)
+        m.delete('/'.join([client.endpoint, 'documents']), status_code=error_code, content=error_content)
 
-    with pytest.raises(error_name):
-        client.create_prediction(document_id, model_id)
+        with pytest.raises(error_name):
+            client.create_document(content, mime_type, consent_id=consent_id)
 
-    with pytest.raises(error_name):
-        client.update_document(document_id, [{'foo': 'bar'}])
+        with pytest.raises(error_name):
+            client.create_prediction(document_id, model_id)
 
-    with pytest.raises(error_name):
-        client.delete_documents(consent_id=consent_id)
+        with pytest.raises(error_name):
+            client.update_document(document_id, [{'foo': 'bar'}])
+
+        with pytest.raises(error_name):
+            client.delete_documents(consent_id=consent_id)
