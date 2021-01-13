209566b9ee1b9295403baf55a67dc24857a9fc42
Magnus Rud <mag.aars@gmail.com>
2021-01-13 11:04:25 +0100

    add test

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -8,3 +8,4 @@ def test_get_log(client: Client):
     log_id = service.create_log_id()
     response = client.get_log(log_id)
     assert 'logId' in response, 'Missing logId in response'
+    assert isinstance(response['events'], list), 'Missing list of events'
