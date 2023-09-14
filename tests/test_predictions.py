276f37d61d2588f76d45857d1c2f55182c528372
augustak <august@lucidtech.ai>
2023-09-14 14:27:02 +0200

    Added model_id to list_predictions

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -32,11 +32,10 @@ def test_create_prediction(client: Client, preprocess_config, postprocess_config
     assert 'predictionId' in response, 'Missing predictionId in response'
 
 
-@pytest.mark.parametrize('sort_by,order', [
-    ('createdTime', 'ascending'),
-    ('createdTime', 'descending'),
-])
-def test_list_predictions(client: Client, sort_by, order):
-    response = client.list_predictions(sort_by=sort_by, order=order)
+@pytest.mark.parametrize('sort_by', ['createdTime', None])
+@pytest.mark.parametrize('order', ['ascending', 'descending', None])
+@pytest.mark.parametrize('model_id', [service.create_model_id(), None])
+def test_list_predictions(client: Client, sort_by, order, model_id):
+    response = client.list_predictions(sort_by=sort_by, order=order, model_id=model_id)
     logging.info(response)
     assert 'predictions' in response, 'Missing predictions in response'
